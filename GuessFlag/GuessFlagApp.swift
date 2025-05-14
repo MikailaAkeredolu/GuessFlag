f56d240cf2401d68a08b6a9d008b2503c757a748
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2025-05-14 07:58:55 -0400

    Initial Commit

---
+++ GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by mikaila Akeredolu on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
